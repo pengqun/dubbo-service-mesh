b5c6b092c6cfeafda6313117f1cabc0e2d8bcb20
楚衡 <pengqun.pq@alibaba-inc.com>
2018-04-18 12:30:05 +0800

    add cpp boilerplate

---
+++ cmake-build-debug/CMakeFiles/cpptest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpptest.dir/main.cpp.o"
+  "cpptest.pdb"
+  "cpptest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpptest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
